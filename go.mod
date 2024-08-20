7326c5564a5be4c7a33ee5de98becca22e5aae38
Mogs91 <markossayan@gmail.com>
2024-08-20 13:10:12 +0200

    setup for wolf

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mogs91/wolf
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package wolf
+
+import(
+	"strings"
+)
+
+func WhenGrownUps(s string)string{
+	return "When the puppy grous up it says: " + strings.ToUpper(s)
+}
